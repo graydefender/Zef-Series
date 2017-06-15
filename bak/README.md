0f8e9a83d66c0992687f06a03ef4b5d2db0bff04
graydefender <graydefender@outlook.com>
2017-06-14 18:42:17 -0700

    06/14/17 Updates for Zef Episode 4

--- bak/README.md
+++ bak/README.md
@@ -6,3 +6,9 @@ This is where I will be saving my source code for this tutorial series.  These p
 Right now the source code consists of one basic program "zef.bas" and a few assembly language programs.
 
 The first one "zef1.asm" was my first cut at converting the basic program into assembler.  The second one, "zef2.asm" is my optimized version.
+
+The third one "zef3.asm" expands upon the second one adding a redefined character set with a shifting character to create that "water" effect.
+
+There is a separate file "shifting.asm" which produces the shifting effect and interrupt in one source file.
+
+"interrupt.asm" provides a quick example of one way to code a program interrupt on the Commodore 64.
--- bak/window.asm
+++ bak/window.asm
@@ -18,11 +18,10 @@ defm                shift_window
                     sta                 HT   
                     jsr Scroll_Window_Up                                     
 endm
-
-                    shift_window        #5,#0,#5,#5   
-                       ; cursor bottom row
-                       ; print text
-          
+                  
+                    shift_window        #5,#0,#10,#5   
+         
+                    
                     rts
                     
 Scroll_Window_Up
@@ -35,27 +34,28 @@ Scroll_Window_Up
                     clc
                     lda                 WIN_Y                                   
                     adc                 HT                                      
-                    sta                 @max_ht+1    
-@loop               ldx                 WIN_X
-                    store_values        Const_Screen_H,@input+2,Const_Screen_L,@input+1                    
-                    store_values        Const_Screen_H,@lastline+2,Const_Screen_L,@lastline+1                    
+                    sta                 max_ht+1    
+loop                ldx                 WIN_X
+                    store_values        Const_Screen_H,input+2,Const_Screen_L,input+1                    
+                    store_values        Const_Screen_H,lastline+2,Const_Screen_L,lastline+1                    
                     dey
-                    store_values        Const_Screen_H,@output+2,Const_Screen_L,@output+1                    
+                    store_values        Const_Screen_H,output+2,Const_Screen_L,output+1                    
                     iny
-@input              lda $428,x
-@output             sta $400,x 
+input               lda $428,x
+output              sta $400,x 
                     lda #$20
-@lastline           sta $428,x
+lastline            sta $428,x
                     inx
                     cpx                 MAX_WIDTH                  
-                    bne                 @input                                  
+                    bne                 input                                  
                     iny
-@max_ht             cpy                 #10                  
-                    bne                 @loop               
-;*********************************
-;* blank out bottom row of window                    
-;*********************************
-                    rts                    
+max_ht              cpy                 #10                  
+                    bne                 loop               
+                    rts
+
+
+string              null                ">NORTH"
+string_pos          byte 00
 ;*********************************                    
 Const_Screen_L      byte                $00,$28,$50,$78,$A0,$C8,$F0,$18,$40,$68,$90,$b8,$E0,$08,$30,$58,$80,$a8,$d0,$f8,$20,$48,$70,$98,$c0
 Const_Screen_H      byte                $04,$04,$04,$04,$04,$04,$04,$05,$05,$05,$05,$05,$05,$06,$06,$06,$06,$06,$06,$06,$07,$07,$07,$07,$07
